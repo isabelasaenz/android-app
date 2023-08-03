de802b5625bc66ebe52492676aaff24c9ade0c4d
isabela <isabelasaenzcardoso@gmail.com>
2023-08-03 17:40:51 -0300

    final version...?

--- app/src/main/java/com/example/todoapp/RecyclerViewTouchHelper.java
+++ app/src/main/java/com/example/todoapp/RecyclerViewTouchHelper.java
@@ -3,6 +3,7 @@ package com.example.todoapp;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Canvas;
+import android.util.TypedValue;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -32,15 +33,15 @@ public class RecyclerViewTouchHelper extends ItemTouchHelper.SimpleCallback {
         final int position = viewHolder.getAdapterPosition();
         if (direction == ItemTouchHelper.RIGHT){
             AlertDialog.Builder builder = new AlertDialog.Builder(adapter.getContext());
-            builder.setTitle("Delete Task");
-            builder.setMessage("Are You Sure ?");
+            builder.setTitle("Delete task");
+            builder.setMessage("Are you sure?");
             builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     adapter.deleteTask(position);
                 }
             });
-            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     adapter.notifyItemChanged(position);
@@ -57,12 +58,14 @@ public class RecyclerViewTouchHelper extends ItemTouchHelper.SimpleCallback {
     public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
 
         new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-                .addSwipeLeftBackgroundColor(ContextCompat.getColor(adapter.getContext() , R.color.colorPrimaryDark))
+                .addSwipeLeftBackgroundColor(ContextCompat.getColor(adapter.getContext(), R.color.colorPrimaryDark))
                 .addSwipeLeftActionIcon(R.drawable.ic_edit)
-                .addSwipeRightBackgroundColor(ContextCompat.getColor(adapter.getContext() , R.color.colorPrimary))
+                .addSwipeRightBackgroundColor(ContextCompat.getColor(adapter.getContext(), R.color.colorPrimary))
                 .addSwipeRightActionIcon(R.drawable.ic_delete)
+                .addCornerRadius(TypedValue.COMPLEX_UNIT_DIP, 8)
                 .create()
                 .decorate();
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
     }
+
 }
